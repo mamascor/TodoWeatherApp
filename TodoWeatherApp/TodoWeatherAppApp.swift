346634f8a341c048300462dc2ae87dd32e0b8042
Marco Mascorro <marco.mascorroa@gmail.com>
2022-12-11 19:38:24 -0600

    Initial Commit

---
+++ TodoWeatherApp/TodoWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoWeatherAppApp.swift
+//  TodoWeatherApp
+//
+//  Created by Marco Mascorro on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
